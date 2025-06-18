8e78fc0fffe512f8ea31e42e2687e96c00191562
Iasmyn <141281664+isavv127@users.noreply.github.com>
2025-06-18 01:40:18 +0000

    test

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
+    char linha[10]= {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    //criando tabuleiro 10x10
+    int tabuleiro [10][10];
+    for (int i=0; i < 10; i++);
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
